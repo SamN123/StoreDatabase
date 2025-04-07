d3f8527311c28c59d4b4f0be9094f329f63f3b35
Jason Gedlu <57841746+JGedlu@users.noreply.github.com>
2025-04-07 11:00:53 -0400

    Update and rename Products.java to Product.java

--- Objects/Product.java
+++ Objects/Product.java
@@ -1,10 +1,10 @@
 public class Product {
-    private int id;
+    private String id;
     private String name;
-    private int price;
+    private double price;
     private int quantity;
 
-    public Product (int productId, String productName, int productPrice, int productQuantity) {
+    public Product (String productId, String productName, double productPrice, int productQuantity) {
         this.id = productId;
         this.name = productName;
         this.price = productPrice;
@@ -12,13 +12,13 @@ public class Product {
     }
 
     // Getters
-    public int getId() { return id; }
+    public String getId() { return id; }
     public String getName() { return name;}
-    public int getPrice() { return price; }
+    public double getPrice() { return price; }
     public int getQuantity() { return quantity; }
 
     // Setters (Insert as needed as project progresses)
-    
+
     @Override
-    public String toString() { return "Insert string here depending on project needs."; }
+    public String toString() { return id + " | " + name + " | $" + price + " | Quantity: " + quantity; }
 }
