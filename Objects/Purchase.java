39ef327aef198a2031b4d5e905a10a4728f6f961
Jason Gedlu <57841746+JGedlu@users.noreply.github.com>
2025-04-07 11:08:35 -0400

    Update Purchase.java

--- Objects/Purchase.java
+++ Objects/Purchase.java
@@ -1,26 +1,24 @@
 public class Purchase {
     private int id;
-    private String name;
-    private int price;
+    private int perID;
+    private String prodID;
     private String date;
     private int quantity;
 
-    public Purchase (int purchaseId, String personID, int productID, String purchaseDate, int purchaseQuantity) {
+    public Purchase (int purchaseId, int personID, String productID, String purchaseDate, int purchaseQuantity) {
         this.id = purchaseId;
-        this.name = personID;
-        this.price = productID;
+        this.perID = personID;
+        this.prodID = productID;
         this.date = purchaseDate;
         this.quantity = purchaseQuantity;
     }
 
     public int getId() { return id; }
-    public String getName() { return name; }
-    public int getPrice() { return price; }
     public String getDate() { return date; }
     public int getQuantity() { return quantity; }
 
     // Setters (Add as project needs demand)
-    
+
     @Override
-    public String toString() { return "Insert string here depending on project needs."; }
+    public String toString() { return id + " | " + prodID + " | " + perID + " x" + quantity + " | " + date; }
 }
