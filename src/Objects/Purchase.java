5641ceb540e914b28874a7c5057de5a8aafec2a1
Jason Gedlu <57841746+JGedlu@users.noreply.github.com>
2025-04-09 09:59:24 -0400

    Create Purchase.java

---
+++ src/Objects/Purchase.java
@@ -0,0 +1,26 @@
+public class Purchase {
+    private int id;
+    private int perID;
+    private String prodID;
+    private String date;
+    private int quantity;
+
+    public Purchase (int purchaseId, int personID, String productID, String purchaseDate, int purchaseQuantity) {
+        this.id = purchaseId;
+        this.perID = personID;
+        this.prodID = productID;
+        this.date = purchaseDate;
+        this.quantity = purchaseQuantity;
+    }
+
+    public int getId() { return id; }
+    public String getDate() { return date; }
+    public int getQuantity() { return quantity; }
+
+    // Setters (Add as project needs demand)
+
+    @Override
+    public String toString() {
+        return String.format("PurchaseID: %d | CustomerID: %d | ProductID: %s | Quantity: %d | Date: %s", id, perID, prodID, quantity, date);
+    }
+}
